09a3b78632427822e10f574e8029ce01030b0f8b
Sebushka89 <sebastian.rizzo@hotmail.it>
2021-06-09 14:08:44 +0200

    PRIMA PARTE ESERCIZIO SVOLTA

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 /*Palidroma
 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 Pari e Dispari
-L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-Sommiamo i due numeri.
-Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-Infine, dichiariamo chi ha vinto.*/
+*/
+
+function checkPalindroma(str){
+  var strInversa = str.split('').reverse().join('');  
+  return strInversa;
+}
+
+var parola = prompt('inserisci qua la tua parola');
+
+var parolaInversa = checkPalindroma(parola);
+
+if(parola == parolaInversa){
+    document.getElementById('pali').innerHTML='La parola è palindroma';
+  } else {
+    document.getElementById('non_pali').innerHTML='La parola non è palindroma';
+  }
+  
+/*  L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+  Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+  Sommiamo i due numeri.
+  Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+  Infine, dichiariamo chi ha vinto.*/
+
+  
